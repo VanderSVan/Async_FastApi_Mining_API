e6b92d2a8c8efb74d62aca370ebfa41189b62dff
vandersvan <vandersvan@gmail.com>
2022-10-22 13:45:17 +0300

    Replace 'condecimal(decimal_places=6)' to 'float'

--- src/api/schemas/content/base_schemas.py
+++ src/api/schemas/content/base_schemas.py
@@ -5,21 +5,16 @@ from pydantic import BaseModel, Field, condecimal
 
 class ContentBaseSchema(BaseModel):
     datetime: dt = Field(..., example=dt.utcnow().strftime('%Y-%m-%dT%H:%M:%S'))
-    iron_percent: condecimal(decimal_places=6) = Field(..., example=66.6)
-    silicon_percent: condecimal(decimal_places=6) = Field(..., example=5.55)
-    aluminum_percent: condecimal(decimal_places=6) = Field(..., example=0.19)
-    calcium_percent: condecimal(decimal_places=6) = Field(..., example=0.25)
-    sulfur_percent: condecimal(decimal_places=6) = Field(..., example=0.05)
+    iron_percent: float = Field(..., example=66.6)
+    silicon_percent: float = Field(..., example=5.55)
+    aluminum_percent: float = Field(..., example=0.19)
+    calcium_percent: float = Field(..., example=0.25)
+    sulfur_percent: float = Field(..., example=0.05)
     ore_concentrate_id: int = Field(..., ge=1)
 
 
 class ContentGetSchema(ContentBaseSchema):
     id: int = Field(..., ge=1)
-    iron_percent: float = Field(...)
-    silicon_percent: float = Field(...)
-    aluminum_percent: float = Field(...)
-    calcium_percent: float = Field(...)
-    sulfur_percent: float = Field(...)
 
     class Config:
         orm_mode = True
@@ -31,11 +26,11 @@ class ContentDeleteSchema(ContentBaseSchema):
 
 class ContentPatchSchema(ContentBaseSchema):
     datetime: dt | None = Field(None, example=dt.utcnow().strftime('%Y-%m-%dT%H:%M:%S'))
-    iron_percent: condecimal(decimal_places=6) | None = Field(None, example=67.385)
-    silicon_percent: condecimal(decimal_places=6) | None = Field(None, example=4.789)
-    aluminum_percent: condecimal(decimal_places=6) | None = Field(None, example=0.2003)
-    calcium_percent: condecimal(decimal_places=6) | None = Field(None, example=0.250005)
-    sulfur_percent: condecimal(decimal_places=6) | None = Field(None, example=0.051234)
+    iron_percent: float | None = Field(None, example=67.385)
+    silicon_percent: float | None = Field(None, example=4.789)
+    aluminum_percent: float | None = Field(None, example=0.2003)
+    calcium_percent: float | None = Field(None, example=0.250005)
+    sulfur_percent: float | None = Field(None, example=0.051234)
     ore_concentrate_id: int | None = Field(None, ge=1)
 
 
